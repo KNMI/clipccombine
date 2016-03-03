0015cb65eca93093012574eaae3de189fbc331bc
andrejsim <andrej@knmi.nl>
2016-03-03 11:20:23 +0100

    changes...

--- clipc_combine_process/clipc_combine_process.py
+++ clipc_combine_process/clipc_combine_process.py
@@ -19,7 +19,7 @@ def defaultCallback(message,percentage):
 # two netCDF files combined by operator into one along all dimmensions, should be same...
 #
 
-def combine_two_indecies_netcdf(netCDF1,netCDF2,operator_symbol,output,callback=defaultCallback):
+def combine_two_indecies_netcdf( netCDF1, netCDF2, operator_symbol, output, norm1, norm2, callback=defaultCallback):
 
   #id data layer (3dims...)
   variable1 = getTitleNC(netCDF1)
@@ -32,22 +32,24 @@ def combine_two_indecies_netcdf(netCDF1,netCDF2,operator_symbol,output,callback=
 
   # define operator functions
   ops = { "+": operator.add , 
-      "-": operator.subtract, 
-      "*": operator.multiply, 
-      "/": operator.divide  }
+          "-": operator.subtract , 
+          "*": operator.multiply , 
+          "/": operator.divide }
 
   # parse operator symbol to function
   op_char = operator_symbol
   op_func = ops[op_char]
   #result = op_func(a, b)
-  combi = combine_netcdf.combineNetCDFs( name, netCDF1 , variable1 , netCDF2 , variable2 , des , op_func,callback=callback)
+
+  combi = combine_netcdf.combineNetCDFs( name, netCDF1 , variable1 , netCDF2 , variable2 , des , op_func, norm1 , norm2 ,callback=callback)
+  
   return combi , name
 
 # end
 
 # two urls opened as netCDF files and combined with operator allong all dimensions
 #
-def combine_two_indecies(url1,url2,operation,output,callback=defaultCallback):
+def combine_two_indecies(url1,url2,operation,output, norm1 , norm2 ,callback=defaultCallback):
 
   nc1 = netCDF4.Dataset(url1,'r')
 
@@ -56,7 +58,7 @@ def combine_two_indecies(url1,url2,operation,output,callback=defaultCallback):
   callback("nc1 time received: "+str(netCDF4.num2date( nc1.variables['time'][0] , nc1.variables['time'].units ,calendar='standard')),3)
   callback("nc2 time received: "+str(netCDF4.num2date( nc2.variables['time'][0] , nc2.variables['time'].units ,calendar='standard')),3)
 
-  nc_combination , combi_name = combine_two_indecies_netcdf(nc1, nc2, operation, output,callback=callback)
+  nc_combination , combi_name = combine_two_indecies_netcdf(nc1, nc2, operation, output, norm1 , norm2 ,callback=callback)
 
   callback("combo time received: "+str(netCDF4.num2date( nc_combination.variables['time'][0] , nc_combination.variables['time'].units ,calendar='standard')),3)
 
@@ -76,13 +78,17 @@ def getTitleNC(nc_fid):
 
 def combine_two_indecies_wcs(wcs_url1, 
   wcs_url2, 
-  operator_symbol, 
+  operator_symbol,
+  norm1 , 
+  norm2 , 
   bbox=None , 
   time1=None , 
   time2=None ,
   output_file1='nc1.nc',
   output_file2='nc2.nc',
   output_file3='combine.nc' ,
+  width=200,
+  height=200,
   callback=defaultCallback):
 
   # wcs combo
@@ -90,16 +96,18 @@ def combine_two_indecies_wcs(wcs_url1,
   defaultCallback('wcs url1: '+str(wcs_url1),1)
   defaultCallback('wcs url2: '+str(wcs_url2),1)
   defaultCallback('bbox:     '+str(bbox),1)
-  defaultCallback('time1:     '+str(time1),1)
-  defaultCallback('time2:     '+str(time2),1)
+  defaultCallback('time1:    '+str(time1),1)
+  defaultCallback('time2:    '+str(time2),1)
+  defaultCallback('height:   '+str(height),1)
+  defaultCallback('width:    '+str(width),1)
 
-  nc1 = wcsrequest.getWCS(wcs_url1, bbox, time1, output_file1,callback=callback)
+  nc1 = wcsrequest.getWCS(wcs_url1, bbox, time1, output_file1,width=width,height=height,callback=callback)
 
-  nc2 = wcsrequest.getWCS(wcs_url2, bbox, time2, output_file2,callback=callback)  
+  nc2 = wcsrequest.getWCS(wcs_url2, bbox, time2, output_file2,width=width,height=height,callback=callback)  
   
   defaultCallback("Combining",2);
   
-  nc_output , nc_out = combine_two_indecies( nc1 , nc2 , operator_symbol , output_file3 ,callback=callback)
+  nc_output , nc_out = combine_two_indecies( nc1 , nc2 , operator_symbol , output_file3 , norm1 , norm2 , callback=callback)
   
   defaultCallback("Combining ready",3);
   
